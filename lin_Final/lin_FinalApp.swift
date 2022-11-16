60336932ac3e21ec7fe427eb937c49aff0814044
tony <linchiah01@gmail.com>
2022-11-16 16:32:18 +0800

    Initial Commit

---
+++ lin_Final/lin_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  lin_FinalApp.swift
+//  lin_Final
+//
+//  Created by User01 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct lin_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
